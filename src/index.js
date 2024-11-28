193b51c848085eb4d5430166a8d04838a7b97f45
Galina Bobrik <galagrinko@gmail.com>
2024-11-28 20:02:36 +0400

    function to get random charecter adde

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
-import MervelService from './services/MervelService';
 import './style/style.scss';
-
-const marvelService = new MervelService();
-marvelService.getAllCharacters().then((res) => res.data.results.forEach((item) => console.log(item.name)));
 
 const container = document.getElementById('root');
 const root = createRoot(container);
