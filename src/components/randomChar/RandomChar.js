b04cc26cd58e3af65e301ac411e41eaaf8c5508a
Galina Bobrik <galagrinko@gmail.com>
2024-12-25 13:15:59 +0400

    feat: add  errorMessage and spinner

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,9 @@
 import { Component } from "react";
-import "./randomChar.scss";
 import MervelService from "../../services/MervelService";
 import mjolnir from "../../resources/img/mjolnir.png";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import "./randomChar.scss";
 
 class RandomChar extends Component {
     constructor(props) {
@@ -11,55 +13,45 @@ class RandomChar extends Component {
 
     state = {
         char: {},
+        loading: true,
+        error: false,
     };
 
     marvelService = new MervelService();
 
     onCharLoaded = (char) => {
-        this.setState({ char });
+        this.setState({
+            char,
+            loading: false,
+        });
+    };
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true,
+        });
     };
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        this.marvelService.getCharacter(id).then(this.onCharLoaded);
+        this.marvelService
+            .getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
     };
 
     render() {
-        const {
-            char: { name, description, thumbnail, homepage, wiki },
-        } = this.state;
-
-        const maxDescriptionLength = 210;
-        const cuttedDescription =
-            description && description.length > maxDescriptionLength
-                ? description.substring(0, maxDescriptionLength) + "..."
-                : description;
+        const { char, loading, error } = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View char={char} /> : null;
 
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img
-                        src={thumbnail}
-                        alt="Random character"
-                        className="randomchar__img"
-                    />
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {" "}
-                            {cuttedDescription ||
-                                "There is no description for this character."}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!
@@ -81,4 +73,37 @@ class RandomChar extends Component {
     }
 }
 
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki } = char;
+    const maxDescriptionLength = 210;
+    const cuttedDescription =
+        description && description.length > maxDescriptionLength
+            ? description.substring(0, maxDescriptionLength) + "..."
+            : description;
+
+    return (
+        <div className="randomchar__block">
+            <img
+                src={thumbnail}
+                alt="Random character"
+                className="randomchar__img"
+            />
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {cuttedDescription ||
+                        "There is no description for this character."}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
 export default RandomChar;
